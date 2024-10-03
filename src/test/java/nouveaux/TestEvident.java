c780977ce63c0a57b26543b671dcabfe951cd4a6
samuel <leumasmontreal@hotmail.com>
2024-10-02 23:22:17 -0400

    Added obvious test to make sure initial setup is OK

---
+++ src/test/java/nouveaux/TestEvident.java
@@ -0,0 +1,13 @@
+package nouveaux;
+
+import com.fasterxml.jackson.core.JUnit5TestBase;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestEvident extends JUnit5TestBase{
+    @Test
+    public void testEvident() {
+        assertEquals(1,1);
+    }
+}
