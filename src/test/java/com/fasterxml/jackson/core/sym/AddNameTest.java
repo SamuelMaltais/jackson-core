117cff9e082b4586da3d657cd177a0a461830efb
oceane-hays <128166831+oceane-hays@users.noreply.github.com>
2024-10-10 11:34:40 -0400

    Cinquieme test revisité

--- src/test/java/com/fasterxml/jackson/core/sym/AddNameTest.java
+++ src/test/java/com/fasterxml/jackson/core/sym/AddNameTest.java
@@ -1,36 +1,95 @@
 package com.fasterxml.jackson.core.sym;
 
 import com.fasterxml.jackson.core.JUnit5TestBase;
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.util.InternCache;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddNameTest extends JUnit5TestBase {
-    protected int[] _hashArea;
-    protected InternCache _interner;
 
     @Test
     void addNameOneRepresentation() throws Exception {
-        final ByteQuadsCanonicalizer root = ByteQuadsCanonicalizer.createRoot(137);
-        ByteQuadsCanonicalizer placeholder = root.makeChildOrPlaceholder(2);
+        ByteQuadsCanonicalizer root = ByteQuadsCanonicalizer.createRoot(137);
+        ByteQuadsCanonicalizer placeholder = root.makeChild(JsonFactory.Feature.collectDefaults());
 
-        String name = "Oceane";
-        int q1 = 2;
+        // Réflexion de méthode pour accéder à  _findOffsetForAdd qui est privée
+        Method method = ByteQuadsCanonicalizer.class.getDeclaredMethod("_findOffsetForAdd", int.class);
+        method.setAccessible(true);  // Permet l'accès à la méthode privée
 
-        String result = placeholder.addName(name,q1);
-        assertEquals("Oceane",result);
+        // Calcul du hash
+        int q1 = 10;
+        int hash = placeholder.calcHash(q1);
+        int offset = (int) method.invoke(placeholder, hash);;
+
+        String name = "Hello";
+
+
+        String result = placeholder.addName(name, q1);
+
+        assertEquals(name, result);
+        /*
+         [com.fasterxml.jackson.core.sym.ByteQuadsCanonicalizer: size=1, hashSize=64, 1/0/0/0 pri/sec/ter/spill (=1), total:1]
+                        placeholder                             longueur    taille      elem  1-> result
+         */
+
+        assertEquals(q1, placeholder._hashArea[offset]);
+
+        assertEquals(1, placeholder._hashArea[offset + 3]);
+
+
+        // Vérifier que le hash (q1) est bien stocké dans la zone de hachage
+        if (offset >= placeholder._names.length) {
+            System.out.println("Offset trop grand pour _names: " + offset);
+        } else {
+            assertEquals(name, placeholder._names[offset >> 2]);
+        }
+
+        // Vérifier l'état des compteurs internes
+        assertEquals(1, placeholder._count);
+
     }
 
     @Test
     void addNameTwoRepresentation() throws Exception {
-        final ByteQuadsCanonicalizer root = ByteQuadsCanonicalizer.createRoot(137);
-        ByteQuadsCanonicalizer placeholder = root.makeChildOrPlaceholder(2);
+        ByteQuadsCanonicalizer root = ByteQuadsCanonicalizer.createRoot(137);
+        ByteQuadsCanonicalizer placeholder = root.makeChild(JsonFactory.Feature.collectDefaults());
 
-        String name = "Oceane";
-        int q1 = 2;
+        // Réflexion de méthode pour accéder à  _findOffsetForAdd qui est privée
+        Method method = ByteQuadsCanonicalizer.class.getDeclaredMethod("_findOffsetForAdd", int.class);
+        method.setAccessible(true);  // Permet l'accès à la méthode privée
+
+        // Calcul du hash
+        int q1 = 10;
         int q2 = 5;
+        int hash = placeholder.calcHash(q1,q2);
+        int offset = (int) method.invoke(placeholder, hash);;
 
-        String result = placeholder.addName(name,q1,q2);
-        assertEquals("Oceane",result);
+        String name = "Hello";
+
+        String result = placeholder.addName(name, q1, q2);
+
+        assertEquals(name, result);
+
+
+        assertEquals(q1, placeholder._hashArea[offset]);
+
+        assertEquals(q2, placeholder._hashArea[offset + 1]);
+
+        assertEquals(2, placeholder._hashArea[offset + 3]);
+
+
+        // Vérifier que le hash (q1) est bien stocké dans la zone de hachage
+        if (offset >= placeholder._names.length) {
+            System.out.println("Offset trop grand pour _names: " + offset);
+        } else {
+            assertEquals(name, placeholder._names[offset >> 2]);
+        }
+
+        // Vérifier l'état des compteurs internes
+        assertEquals(1, placeholder._count);
     }
 }
